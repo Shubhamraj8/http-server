63f17934ab162912703ab8bafef862d6bca0d41d
Shubham Raj Rathaur <94780982+Shubhamraj8@users.noreply.github.com>
2026-01-09 17:33:14 +0000

    intro to express framework

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-const http = require ("http");
-const fs = require("fs");
-const url = require("url");
-const myServer = http.createServer((req, res) => {
-    if(req.url==='/favicon.ico') return res.end();
-    const log = `${Date.now()}: ${req.method} ${req.url} New Request Received!\n`
-    const myUrl = url.parse(req.url, true);
-    fs.appendFile("log.txt", log, (err) => {
-        switch(myUrl.pathname){
-            case "/":
-                if(req.method === "GET") res.end("Home");
-                break;
-            case "/about":
-                const username = myUrl.query.myname;
-                res.end(`Hi, ${username}`);
-                break;
-            case "/search":
-                const search = myUrl.query.search_query;
-                res.end("Here are your results for "+ search);
-            case "/signup":
-                if(req.method === "GET") res.end("This is a SignUp Form");
-                else if(req.method === "POST"){
-                    //DB Query
-                    res.end("Success");
-                }
-            default:
-                res.end("404 Not Found");
-        }
-    });
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send( "Hello from Homepage")
 });
-myServer.listen(3000, () => console.log("Server Started!"));
+
+app.get("'/about", (req, res) => {
+    return res.send(`Hello ${req.query.name}`);
+});
+
+app.listen(3000, () => console.log("Server Started!"));
