41da32ec784cac49927fc72f9601c8a7cefb3fdd
Shubham Raj Rathaur <94780982+Shubhamraj8@users.noreply.github.com>
2026-01-09 12:01:53 +0000

    used http methods

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const fs = require("fs");
 const url = require("url");
 const myServer = http.createServer((req, res) => {
     if(req.url==='/favicon.ico') return res.end();
-    const log = `${Date.now()}: ${req.url} New Request Received!\n`
+    const log = `${Date.now()}: ${req.method} ${req.url} New Request Received!\n`
     const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
     fs.appendFile("log.txt", log, (err) => {
         switch(myUrl.pathname){
             case "/":
-                res.end("Home");
+                if(req.method === "GET") res.end("Home");
                 break;
             case "/about":
                 const username = myUrl.query.myname;
@@ -18,6 +17,12 @@ const myServer = http.createServer((req, res) => {
             case "/search":
                 const search = myUrl.query.search_query;
                 res.end("Here are your results for "+ search);
+            case "/signup":
+                if(req.method === "GET") res.end("This is a SignUp Form");
+                else if(req.method === "POST"){
+                    //DB Query
+                    res.end("Success");
+                }
             default:
                 res.end("404 Not Found");
         }
